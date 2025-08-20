d954caef76bea4e460c84cab405f9d518ab2fbae
taneason <154041813+taneason@users.noreply.github.com>
2025-08-21 00:49:25 +0800

    change role entity to enum

--- osbuzz.sql
+++ osbuzz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 20, 2025 at 05:59 PM
+-- Generation Time: Aug 20, 2025 at 06:48 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -46,9 +46,9 @@ CREATE TABLE `product` (
 
 INSERT INTO `product` (`product_id`, `product_name`, `brand`, `category`, `price`, `description`, `photo`, `created_at`) VALUES
 (1, 'Way Of Wade 10', 'Lining', 'Casual', 699.00, '', '68a58e71d37ce.jpg', '2025-08-10 15:32:28'),
-(2, 'Ultraboost', 'Adidas', 'Shoes', 459.00, 'Comfortable running shoes', NULL, '2025-08-10 15:32:28'),
-(3, 'Classic Sneakers', 'Nike', 'Shoes', 399.00, 'Classic style everyday sneakers', NULL, '2025-08-10 15:32:28'),
-(4, 'Kids Runner', 'Puma', 'Shoes', 199.00, 'Lightweight kids running shoes', NULL, '2025-08-10 15:32:28');
+(2, 'Ultraboost', 'Adidas', 'Formal', 459.00, 'Comfortable running shoes', NULL, '2025-08-10 15:32:28'),
+(3, 'Classic Sneakers', 'Nike', 'Basketball', 399.00, 'Classic style everyday sneakers', NULL, '2025-08-10 15:32:28'),
+(4, 'Kids Runner', 'Puma', 'Other', 199.00, 'Lightweight kids running shoes', NULL, '2025-08-10 15:32:28');
 
 -- --------------------------------------------------------
 
@@ -90,7 +90,7 @@ CREATE TABLE `user` (
   `address` text NOT NULL,
   `phone` varchar(20) NOT NULL,
   `photo` varchar(255) NOT NULL,
-  `role` varchar(100) NOT NULL
+  `role` enum('Admin','Member') NOT NULL DEFAULT 'Member'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -99,10 +99,10 @@ CREATE TABLE `user` (
 
 INSERT INTO `user` (`id`, `username`, `email`, `password`, `name`, `address`, `phone`, `photo`, `role`) VALUES
 (13, 'eason', 'taneason1111@gmail.com', '7c222fb2927d828af22f592134e8932480637c0d', 'Tan Ea Son', 'No 6 Jalan Indah 10/5Taman Pertama', '0122226133', '6898d553e9814.jpg', 'Admin'),
-(14, '123123', 'taneason0912@gmail.com', '697f6f62764c05183042401e6bc74c6704a3da7d', '', 'sdasd', '0177438690', '687bcf112e8e1.jpg', ''),
-(15, 'Tan1221', 'taneason0000@gmail.com', '7c222fb2927d828af22f592134e8932480637c0d', '', '', '', '687ce54d6c885.jpg', ''),
-(16, '0123456789', '12@gmail.com', '4f409ed0d5a586b3fbd255922b5afc6eec549dc9', 'gdf', '', '017-743 8690', '68986494cf85f.jpg', ''),
-(17, 'test123123123', 'eason@gmail.com', '88ea39439e74fa27c09a4fc0bc8ebe6d00978392', '', '', '', '68a5ac56994ab.jpg', '');
+(14, '123123', 'taneason0912@gmail.com', '697f6f62764c05183042401e6bc74c6704a3da7d', '', 'sdasd', '0177438690', '687bcf112e8e1.jpg', 'Member'),
+(15, 'Tan1221', 'taneason0000@gmail.com', '7c222fb2927d828af22f592134e8932480637c0d', '', '', '', '687ce54d6c885.jpg', 'Member'),
+(16, '0123456789', '12@gmail.com', '4f409ed0d5a586b3fbd255922b5afc6eec549dc9', 'gdf', '', '017-743 8690', '68986494cf85f.jpg', 'Member'),
+(17, 'test123123123', 'eason@gmail.com', '88ea39439e74fa27c09a4fc0bc8ebe6d00978392', '', '', '', '68a5ac56994ab.jpg', 'Member');
 
 --
 -- Indexes for dumped tables
